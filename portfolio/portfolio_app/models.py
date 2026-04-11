6b65356c37200fe90f892cfc6e3e19e4b0e3467e
Blastermore <afonsocunha.dev@gmail.com>
2026-04-11 03:59:48 +0100

    Model pa Unidade Curricular

--- portfolio/portfolio_app/models.py
+++ portfolio/portfolio_app/models.py
@@ -11,3 +11,19 @@ class Licenciatura(models.Model):
 
     def __str__(self):
         return self.nome
+
+#Modelo para uma Unidade Curricular
+class UnidadeCurricular(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    creditos = models.IntegerField()
+    avaliacao = models.IntegerField()
+    ano = models.IntegerField()
+    semestre = models.IntegerField()
+    capa = models.ImageField(upload_to='capa_unidades/')
+
+    licenciatura = models.ForeignKey(Licenciatura, on_delete=models.CASCADE)
+    docentes = models.ManyToManyField('Docente')
+
+    def __str__(self):
+        return self.nome
