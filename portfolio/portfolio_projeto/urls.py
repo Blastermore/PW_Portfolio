5441a40f8106fd2fa9ec93135b0e2072a330cf99
Blastermore <103764900+Blastermore@users.noreply.github.com>
2026-04-25 01:19:34 +0000

    Add escola/url.py

--- portfolio/portfolio_projeto/urls.py
+++ portfolio/portfolio_projeto/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("escola/", admin.site.urls), #rota para aplicação web escola
+    path("escola/", include("escola.url")), #rota para aplicação web escola
 ]
 
 if settings.DEBUG:
